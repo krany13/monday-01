e0656927a02e60e1c3f1b0a87f08eadb0c92f6af
Vladimir <krasniy.vladim@yandex.ru>
2022-09-15 21:23:23 +0300

    fix post(return)

--- src/middlewares/input-validations-middleware.ts
+++ src/middlewares/input-validations-middleware.ts
@@ -5,5 +5,7 @@ export const inputValidationsMiddleware = (req: Request, res: Response, next: Ne
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errorsMessages: [{"message":"error", "field":"error"}] });
+    } else {
+        next()
     }
 }
