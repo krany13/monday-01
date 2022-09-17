39094abc076cdf72a988596cd42573210a5dde13
Vladimir <krasniy.vladim@yandex.ru>
2022-09-17 12:17:56 +0300

    inputValidationsMiddleware

--- src/routes/videos-router.ts
+++ src/routes/videos-router.ts
@@ -6,20 +6,22 @@ import TypedArray = NodeJS.TypedArray;
 
 export const videoRouter = Router({})
 
-const titleValidations = body('title').isString().notEmpty().isLength({max: 40 })
-const authorValidations = body('author').isString().notEmpty().isLength({max:20})
-const availableResolutionsValidations = body('availableResolutions').isArray().notEmpty().isLength({max: 8})
+const stdResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
+
+const titleValidations = body('title').isString().notEmpty().isLength({max: 40})
+const authorValidations = body('author').isString().notEmpty().isLength({max: 20})
+const availableResolutionsValidations = body('availableResolutions').isArray().notEmpty().isLength({max: 8}).isIn(stdResolutions)
 const canBeDownloadedValidations = body('canBeDownloaded').isBoolean()
 const minAgeRestrictionValidations = body('minAgeRestriction').isNumeric()
 
-videoRouter.get('/', (req: Request, res:Response) => {
+videoRouter.get('/', (req: Request, res: Response) => {
     const findVideos = videosRepository.seeVideo()
     return res.status(200).send(findVideos)
 })
 
-videoRouter.get('/:id', (req: Request, res:Response) => {
+videoRouter.get('/:id', (req: Request, res: Response) => {
     let video = videosRepository.findVideoById(+req.params.id)
-    if(video) {
+    if (video) {
         return res.send(video)
     } else {
         return res.sendStatus(404)
@@ -32,9 +34,9 @@ videoRouter.get('/:id', (req: Request, res:Response) => {
 //     // res.send(204)
 // })
 //сделал
-videoRouter.delete('/:id', (req: Request, res:Response) => {
-    const isDeleted =  videosRepository.deleteVideoById(+req.params.id)
-    if(isDeleted) {
+videoRouter.delete('/:id', (req: Request, res: Response) => {
+    const isDeleted = videosRepository.deleteVideoById(+req.params.id)
+    if (isDeleted) {
         return res.sendStatus(204)
     } else {
         return res.sendStatus(404)
@@ -45,9 +47,8 @@ videoRouter.post('/',
     titleValidations,
     authorValidations,
     availableResolutionsValidations,
-    //TODO: добавить валидацию для разрешений
     inputValidationsMiddleware,
-    (req: Request, res:Response) => {
+    (req: Request, res: Response) => {
         const newVideo = videosRepository.createVideo(req.body.title, req.body.author, req.body.availableResolutions)
         return res.status(201).send(newVideo)
     })
@@ -61,10 +62,10 @@ videoRouter.put('/:id',
     canBeDownloadedValidations,
     minAgeRestrictionValidations,
     inputValidationsMiddleware,
-    (req: Request, res:Response) => {
+    (req: Request, res: Response) => {
         const isUpdated = videosRepository.updateVideo(+req.params.id, req.body.title, req.body.author)
-        if(isUpdated) {
-            const video =  videosRepository.findVideoById(+req.params.id)
+        if (isUpdated) {
+            const video = videosRepository.findVideoById(+req.params.id)
             return res.status(204).send(video)
         } else {
             return res.sendStatus(404)
